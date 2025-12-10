38a8502050e484989b761845294a88d0b1dd9fff
Sathpreethika <sathpreethika23@gmail.com>
2025-12-10 16:52:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TrainTicket from "./FullStack/Reactjs/TrainTicket";
+
+export default function App() {
+  return (
+    <>
+      <TrainTicket />
+    </>
+  );
+}
+
+
+
